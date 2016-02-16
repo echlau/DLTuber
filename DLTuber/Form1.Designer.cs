8246e237da5dbcb94077ff89f6cc773e87b3137d
manish mallavarapu <manish101096@gmail.com>
2016-02-16 14:47:24 -0800

    added title to video

--- DLTuber/Form1.Designer.cs
+++ DLTuber/Form1.Designer.cs
@@ -32,14 +32,14 @@
             this.selectVideo = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.videoThumbNail = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.vidTitle = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.mediaTypeBox = new System.Windows.Forms.GroupBox();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.fileLocationDialog = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.videoThumbNail)).BeginInit();
             this.mediaTypeBox.SuspendLayout();
             this.SuspendLayout();
@@ -58,9 +58,9 @@
             this.selectVideo.Location = new System.Drawing.Point(304, 39);
             this.selectVideo.Margin = new System.Windows.Forms.Padding(2);
             this.selectVideo.Name = "selectVideo";
-            this.selectVideo.Size = new System.Drawing.Size(95, 20);
+            this.selectVideo.Size = new System.Drawing.Size(95, 19);
             this.selectVideo.TabIndex = 1;
-            this.selectVideo.Text = "Select Video";
+            this.selectVideo.Text = "Download Video";
             this.selectVideo.UseVisualStyleBackColor = true;
             this.selectVideo.Click += new System.EventHandler(this.handleVideoClick);
             // 
@@ -83,14 +83,14 @@
             this.videoThumbNail.TabIndex = 3;
             this.videoThumbNail.TabStop = false;
             // 
-            // label1
+            // vidTitle
             // 
-            this.label1.Location = new System.Drawing.Point(9, 83);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(390, 23);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Title";
+            this.vidTitle.Location = new System.Drawing.Point(9, 83);
+            this.vidTitle.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.vidTitle.Name = "vidTitle";
+            this.vidTitle.Size = new System.Drawing.Size(390, 23);
+            this.vidTitle.TabIndex = 4;
+            this.vidTitle.Text = "Title";
             // 
             // label2
             // 
@@ -111,15 +111,6 @@
             this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 8;
             this.label3.Text = "Youtube URL:";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(404, 39);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 20);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Open Location";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // mediaTypeBox
             // 
@@ -157,6 +148,7 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
             this.radioButton1.Location = new System.Drawing.Point(18, 9);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(45, 17);
@@ -171,10 +163,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 351);
             this.Controls.Add(this.mediaTypeBox);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.vidTitle);
             this.Controls.Add(this.videoThumbNail);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.selectVideo);
@@ -196,14 +187,14 @@
         private System.Windows.Forms.Button selectVideo;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.PictureBox videoThumbNail;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label vidTitle;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox mediaTypeBox;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.SaveFileDialog fileLocationDialog;
     }
 }
 
--- DLTuber/Form1.cs
+++ DLTuber/Form1.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Net;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using System.Net;
+using YoutubeExtractor;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Collections.Specialized;
+using System.Web;
 
 namespace DLTuber
 {
     public partial class Form1 : Form
     {
+        private string title; 
+
+        //private byte[] downloadedByteStream;
+        //private IEnumerable<VideoInfo> videoInfos; 
+        //private VideoDownloader downloader = new VideoDownloader(); 
         public Form1()
         {
             InitializeComponent();
@@ -14,16 +23,21 @@ namespace DLTuber
             progressBar1.Increment(40);
         }
         private bool isValidUrl(string url)
-        {
+        {            
             bool flag = false; 
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.Method = "HEAD";
+            
             try
             {
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+                request.Method = "HEAD";
                 using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
                     flag = response.ResponseUri.ToString().Contains("youtube.com") ? true : false;
             }                
             catch(WebException e)
+            {
+                return flag; 
+            }
+            catch(UriFormatException e)
             {
                 return flag; 
             }
@@ -34,12 +48,35 @@ namespace DLTuber
             try
             {
                 videoThumbNail.Load("https://i.ytimg.com/vi/" + url + "/mqdefault.jpg");
+                title = GetTitle(url);
+                vidTitle.Text = "Title: " + title;
             } 
             catch(WebException e)
             {
                 //videoThumbNail.Image = System.Drawing.Image.ErrorImage; 
             }
-            MessageBox.Show(videoThumbNail.Image.Size.Width + " " + videoThumbNail.Image.Size.Height);
+            
+        }
+        private string openFileLocation()
+        {
+            string dir = " ";
+            DialogResult res = fileLocationDialog.ShowDialog(); 
+            if(res == DialogResult.OK)
+            {
+                dir = Path.GetDirectoryName(fileLocationDialog.FileName); 
+            }
+
+           /* FolderBrowserDialog currentPath = new FolderBrowserDialog();
+            fileLoc.InitialDirectory = currentPath.SelectedPath;
+            DialogResult res = fileLoc.ShowDialog();
+            if (res == DialogResult.OK)
+                dir = fileLoc.FileName; */ 
+
+            return dir; 
+        }
+        private void downloadVideo()
+        {
+            //Parameter for video type
         }
         private void handleVideoClick(object sender, EventArgs e)
         {
@@ -47,12 +84,49 @@ namespace DLTuber
             string url = urlBox.Text; 
             if(isValidUrl(url))
             {
-                loadThumbNail(url.Split('=')[1]); 
+                loadThumbNail(url.Split('=')[1]);
+                //string dir = openFileLocation();
+                //MessageBox.Show(dir); 
+                if (radioButton1.Checked)
+                {
+                    MessageBox.Show(radioButton1.Text);
+                }
+                else if (radioButton2.Checked)
+                {
+                    MessageBox.Show(radioButton2.Text);
+                }
+                else
+                {
+                    
+                    MessageBox.Show(radioButton3.Text);
+                }
             }
+            //Switch statement for radio buttons
             else
             {
+                vidTitle.Text = "Title";
+                videoThumbNail.Image = null;
                 MessageBox.Show("Not a valid Youtube URL please try again "); 
             }
+        }
+        public string GetTitle(string url)
+        {
+            WebClient client = new WebClient();
+            return GetArgs(client.DownloadString("http://youtube.com/get_video_info?video_id=" + url), "title", '&');
+        }
+
+        private string GetArgs(string args, string key, char query)
+        {
+            int iqs = args.IndexOf(query);
+            string querystring = null;
+
+            if (iqs != -1)
+            {
+                querystring = (iqs < args.Length - 1) ? args.Substring(iqs + 1) : String.Empty;
+                NameValueCollection nvcArgs = HttpUtility.ParseQueryString(querystring);
+                return nvcArgs[key];
+            }
+            return String.Empty; // or throw an error
         }
     }
 
